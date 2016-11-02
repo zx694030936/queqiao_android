f414942ca10906196b5ed592b2915a327f9b25c5
Android766 <1355575669@qq.com>
2016-11-02 17:37:47 +0800

    jxd1102

--- app/src/main/java/com/queqiaolove/adapter/queqiao/PartnerGalleryAdapter.java
+++ app/src/main/java/com/queqiaolove/adapter/queqiao/PartnerGalleryAdapter.java
@@ -1,6 +1,8 @@
 package com.queqiaolove.adapter.queqiao;
 
 import android.content.Context;
+
+
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
